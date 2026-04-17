aa1f7b97255f51ffb99b5d7c2c4e426923f10fec
Abhishek Kumar <manovaidya2@gmail.com>
2026-04-17 12:53:48 +0530

    update

--- autism/src/api/axiosInstance.js
+++ autism/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5008/api",
-   baseURL: "https://autismapi.manovaidya.com/api",
+  baseURL: "https://autismapi.manovaidya.com/api",
   headers: {
     "Content-Type": "application/json",
   },
